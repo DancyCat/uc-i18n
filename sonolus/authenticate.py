aa83a9e1fbe819a820a1622d8145f52d1a8d51f9
YumYummity <103061664+YumYummity@users.noreply.github.com>
2025-10-30 20:43:18 -0400

    fix: authenticate should not break even if everything else does

--- sonolus/authenticate.py
+++ sonolus/authenticate.py
@@ -40,7 +40,7 @@ def load_public_key(jwk_dict):
     return pub_key
 
 
-@router.post("/")
+@router.post("")
 async def main(request: Request, data: ServerAuthenticateRequest):
     """
     We support a maximum of 6 sessions:
